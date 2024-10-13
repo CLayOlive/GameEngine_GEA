4404514a12a8e44eb4a4040b7ac71433eb8637eb
CLayOlive <cristianlaynezbachez@gmail.com>
2024-10-13 01:36:17 -0600

    Collisions For Level1 Ready.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 //#include "Games/BreakOut.h"
 //#include "DemoGame/DemoGame.h"
-#include "Games/Level2.h"
+//#include "Games/Level2.h"
+#include "Games/SamuraisVsMongols_Lvl1.h"
 
 int main() {
   //BreakOut demo = BreakOut();
   //DemoGame demo = DemoGame();
-  SamuraisVsMongols demo = SamuraisVsMongols();
+  SamuraisVsMongols_Lvl1 demo = SamuraisVsMongols_Lvl1();
+  //SamuraisVsMongols demo = SamuraisVsMongols();
   demo.setup();
   demo.run();
   return 0;
